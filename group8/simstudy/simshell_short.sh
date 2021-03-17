c187f409b4e554fe1179156f249552f85b9e8d7d
Eva Marie Tourangeau <tourange@lanl.gov>
2021-03-17 12:42:06 -0700

    reducing q (and thus q_obs)

--- group8/simstudy/simshell_short.sh
+++ group8/simstudy/simshell_short.sh
@@ -3,17 +3,15 @@
 #### Our model
 # Under correctly specified model, vary data parameters
 # Vary q, q_obs=7, N_samp=10 (defaults)
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=25
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=50
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=200
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=20
+# q=5, q_obs=3, N_samp=10 (default)
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=5 --q_obs=3
 
-# Vary q_obs, q=100 (fixed), N_samp=10 (default)
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --q_obs=5
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --q_obs=10
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --q_obs=15
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --q_obs=20
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --q_obs=50
+# Vary q_obs, q=20 (fixed), N_samp=10 (default)
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=20 --q_obs=5
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=20 --q_obs=10
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=20 --q_obs=15
 
 # Vary N_samp, q=10, q_obs=7 (defaults)
 Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=5
@@ -23,26 +21,25 @@ Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_
 Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=100
 
 # Some combinations
-# Vary N_samp, q=100 (fixed), q_obs=7 (default)
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=5 --q=100
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=5 --q=100
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=20 --q=100
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=50 --q=100
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=100 --q=100
+# Vary N_samp, q=15, q_obs=7 (defaults)
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=5 --q=15
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=20 --q=15
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=50 --q=15
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=100 --q=15
 
 # Vary sim-name (misspecify model)
-# Default: N_subj=5, N_samp=10, q=100, q_obs=7
-Rscript model_misspec_sim.R --sim-name="misspec-gamma-gamma-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100
-Rscript model_misspec_sim.R --sim-name="misspec-halft-gamma-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100
-Rscript model_misspec_sim.R --sim-name="misspec-gamma-halft-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100
-Rscript model_misspec_sim.R --sim-name="misspec-halft-halft-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100
-Rscript model_misspec_sim.R --sim-name="misspec-normal-gamma-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100
-Rscript model_misspec_sim.R --sim-name="misspec-normal-halft-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100
-Rscript model_misspec_sim.R --sim-name="misspec-gamma-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100
-Rscript model_misspec_sim.R --sim-name="misspec-halft-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100
-Rscript model_misspec_sim.R --sim-name="misspec-normal-normal-poisson-dirimult" --corr_within=1 --iter=10000 --warmup=5000 --q=100
-Rscript model_misspec_sim.R --sim-name="misspec-normal-normal-negbin-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100
-Rscript model_misspec_sim.R --sim-name="misspec-normal-normal-negbin-dirimult" --corr_within=1 --iter=10000 --warmup=5000 --q=100
+# Default: N_subj=5, N_samp=10, q=15, q_obs=7
+Rscript model_misspec_sim.R --sim-name="misspec-gamma-gamma-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15
+Rscript model_misspec_sim.R --sim-name="misspec-halft-gamma-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15
+Rscript model_misspec_sim.R --sim-name="misspec-gamma-halft-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15
+Rscript model_misspec_sim.R --sim-name="misspec-halft-halft-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15
+Rscript model_misspec_sim.R --sim-name="misspec-normal-gamma-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15
+Rscript model_misspec_sim.R --sim-name="misspec-normal-halft-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15
+Rscript model_misspec_sim.R --sim-name="misspec-gamma-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15
+Rscript model_misspec_sim.R --sim-name="misspec-halft-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15
+Rscript model_misspec_sim.R --sim-name="misspec-normal-normal-poisson-dirimult" --corr_within=1 --iter=10000 --warmup=5000 --q=15
+Rscript model_misspec_sim.R --sim-name="misspec-normal-normal-negbin-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15
+Rscript model_misspec_sim.R --sim-name="misspec-normal-normal-negbin-dirimult" --corr_within=1 --iter=10000 --warmup=5000 --q=15
 
 #### Amy's model
 # Vary sim-name to change data generating process (misspecify model)
@@ -62,17 +59,15 @@ Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_
 
 # Under correctly specified model, vary data parameters
 # Vary q, q_obs=7, N_samp=10 (defaults)
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=25 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=50 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=200 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=20 --N_samp=1
+# q=5, q_obs=3, N_samp=10 (default)
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=5 --q_obs=3 --N_samp=1
 
-# Vary q_obs, q=100 (fixed), N_samp=10 (default)
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --q_obs=5 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --q_obs=10 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --q_obs=15 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --q_obs=20 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --q_obs=50 --N_samp=1
+# Vary q_obs, q=20 (fixed), N_samp=10 (default)
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=20 --q_obs=5 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=20 --q_obs=10 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=20 --q_obs=15 --N_samp=1
 
 # Vary N_samp, q=10, q_obs=7 (defaults)
 Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=5 --N_samp=1
@@ -82,23 +77,22 @@ Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_
 Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=100 --N_samp=1
 
 # Some combinations
-# Vary N_samp, q=100 (fixed), q_obs=7 (default)
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=5 --q=100
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=5 --q=100
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=20 --q=100
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=50 --q=100
-Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=100 --q=100
+# Vary N_samp, q=15, q_obs=7 (defaults)
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=5 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=20 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=50 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="spec-normal-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --N_samp=100 --q=15 --N_samp=1
 
 # Vary sim-name (misspecify model)
-# Default: N_subj=5, N_samp=10, q=100, q_obs=7
-Rscript model_misspec_sim.R --sim-name="misspec-gamma-gamma-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="misspec-halft-gamma-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="misspec-gamma-halft-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="misspec-halft-halft-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="misspec-normal-gamma-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="misspec-normal-halft-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="misspec-gamma-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="misspec-halft-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="misspec-normal-normal-poisson-dirimult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="misspec-normal-normal-negbin-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --N_samp=1
-Rscript model_misspec_sim.R --sim-name="misspec-normal-normal-negbin-dirimult" --corr_within=1 --iter=10000 --warmup=5000 --q=100 --N_samp=1
+# Default: N_subj=5, N_samp=10, q=15, q_obs=7
+Rscript model_misspec_sim.R --sim-name="misspec-gamma-gamma-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="misspec-halft-gamma-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="misspec-gamma-halft-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="misspec-halft-halft-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="misspec-normal-gamma-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="misspec-normal-halft-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="misspec-gamma-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="misspec-halft-normal-poisson-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="misspec-normal-normal-poisson-dirimult" --corr_within=1 --iter=10000 --warmup=5000 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="misspec-normal-normal-negbin-mult" --corr_within=1 --iter=10000 --warmup=5000 --q=15 --N_samp=1
+Rscript model_misspec_sim.R --sim-name="misspec-normal-normal-negbin-dirimult" --corr_within=1 --iter=10000 --warmup=5000 --q=15 --N_samp=1
