3d0c5dd9e6e9a0e68e2f477f3ef50e06ad7bea17
Eva Marie Tourangeau <tourange@lanl.gov>
2021-03-18 11:23:39 -0700

    nsamp figure files

--- group8/sim-vis/N_samp.R
+++ group8/sim-vis/N_samp.R
@@ -1,61 +1,81 @@
+library(ggplot2)
 setwd("./group8/")
 source("./sim-vis/extractinits.R")
+source("./sim-vis/simfns.R")
 
 # Vary N_samp, q=10, q_obs=7, N_subj=5
+N_subj <- 5
 q <- 10
 qobs <- 7
-N_subj <- 5
 
 N_samp <- c(5, 10, 15, 20)
 
-param <- 5
-getsim <- function(param){
-  folder <- paste("spec-normal-normal-poisson-mult/n_5/n_", 
-                  as.character(param),
-                  "/q_10/q_obs_7", sep = "")
-  
-  #truemu <- matrix()
-  # N_samp= 5
-  data <- readRDS(paste("./sim-data/",folder,"/predict_qpcr_data_jobid_10_ad_0.850000_mt_15_ab_1.rds", sep = ""))
-  # Our model
-  omod <- readRDS(paste("./sim-data/", folder, "/predict_qpcr_mod_jobid_10_ad_0.850000_mt_15_ab_1.rds", sep = ""))
-  osamps <- readRDS(paste("./sim-data/", folder, "/predict_qpcr_samps_jobid_10_ad_0.850000_mt_15_ab_1.rds", sep = ""))
-  # Amy's model
-  amod <- readRDS(paste("./sim-data/", folder, "/predict_qpcr_mod_jobid_10_ad_0.850000_mt_15_ab_1_Amy.rds", sep = ""))
-  asamps <- readRDS(paste("./sim-data/", folder, "/predict_qpcr_samps_jobid_10_ad_0.850000_mt_15_ab_1_Amy.rds", sep = ""))
-  
-  return(list(data = data, omod = omod, osamps = osamps, amod = amod, asamps = asamps))
-}
-
-getrmses <- function(param){
-  mods <- getsim(param)
-  # True mus
-  truemu <- mods$data$mu
-  # Model mu estimates
-  omu <- mods$omod[1:(N_subj*param*q), 1]
-  omu <- matrix(omu, ncol = q, byrow = TRUE)
-  amu <- mods$amod[1:(N_subj*param*q), 1]
-  amu <- matrix(amu, ncol = q, byrow = TRUE)
-  nmu <- inits(mods$data$W, mods$data$V, n_samp = param)
-  nmu <- exp(nmu[[1]]$log_mu_tilde)
-  
-  odiff <- truemu - omu
-  ormse <- sqrt(1/(N_samp[1]*N_subj*q)*sum(odiff^2))
-  adiff <- truemu - amu
-  armse <- sqrt(1/(N_samp[1]*N_subj*q)*sum(adiff^2))
-  ndiff <- truemu - nmu
-  nrmse <- sqrt(1/(N_samp[1]*N_subj*q)*sum(ndiff^2))
-  return(c(ormse, armse, nrmse))
-}
 
 rmses <- matrix(nrow = 3, ncol = length(N_samp))
 for (i in 1:length(N_samp)){
-  param <- N_samp[i]
-  rmses[, i] <- getrmses(param)
+  nsamp <- N_samp[i]
+  rmses[, i] <- getrmses(nsamp, q, qobs)
 }
 
-# Credible and prediction intervals
-opostsum <- extract_posterior_summaries(omod, osamps, 1:q)
-apostsum <- extract_posterior_summaries(amod, asamps, 1:q)
+rmspes <- matrix(nrow = 3, ncol = length(N_samp))
+for (i in 1:length(N_samp)){
+  nsamp <- N_samp[i]
+  rmspes[, i] <- getrmspes(nsamp, q, qobs)
+}
+
+coveragemu <- matrix(nrow = 2, ncol = length(N_samp))
+for (i in 1:length(N_samp)){
+  nsamp <- N_samp[i]
+  coveragemu[, i] <- getmucoverage(nsamp, q, qobs)
+}
+
+coveragev <- matrix(nrow = 2, ncol = length(N_samp))
+for (i in 1:length(N_samp)){
+  nsamp <- N_samp[i]
+  coveragev[, i] <- getvcoverage(nsamp, q, qobs)
+}
+
+plot_rmses <- data.frame(rmse = c(t(rmses)),
+                          N_samp = rep(N_samp, 3), 
+                          Estimator = factor(rep(c("Proposed", "Williamson et al.", "Naive"), each = 4), 
+                                             levels = c("Naive", "Williamson et al.", "Proposed")
+                                             )
+                         )
+RMSEplot <- plot_rmses %>% ggplot(aes(x = N_samp, y = rmse, color = Estimator)) + 
+  geom_point() +
+  scale_color_manual(values = c("#E7B800", "#2E9FDF", "#FC4E07")) +
+  xlab("m") + ylab("RMSE") + ggtitle("RMSE")
+
+plot_rmspes <- data.frame(rmspe = c(t(rmspes)),
+                         N_samp = rep(N_samp, 3), 
+                         Estimator = factor(rep(c("Proposed", "Williamson et al.", "Naive"), each = 4), 
+                                            levels = c("Naive", "Williamson et al.", "Proposed")
+                                            )
+                         )
+RMSPEplot <- plot_rmspes %>% ggplot(aes(x = N_samp, y = rmspe, color = Estimator)) + 
+              geom_point() +
+              scale_color_manual(values = c("#E7B800", "#2E9FDF", "#FC4E07")) +
+              xlab("m") + ylab("RMSPE") + ggtitle("RMSPE")
 
 
+plot_covmu <- data.frame(cov = c(t(coveragemu)),
+                          N_samp = rep(N_samp, 2), 
+                          Estimator = factor(rep(c("Proposed", "Williamson et al."), each = 4), 
+                                             levels = c("Williamson et al.", "Proposed")
+                                             )
+                          )
+covmuplot <- plot_covmu %>% ggplot(aes(x = N_samp, y = cov, color = Estimator)) + 
+  geom_point() +
+  scale_color_manual(values = c("#2E9FDF", "#FC4E07")) +
+  xlab("m") + ylab("Coverage") + ggtitle(expression(paste("Interval coverage for ", mu)))
+
+plot_covv <- data.frame(cov = c(t(coveragev)),
+                         N_samp = rep(N_samp, 2), 
+                         Estimator = factor(rep(c("Proposed", "Williamson et al."), each = 4), 
+                                            levels = c("Williamson et al.", "Proposed")
+                                            )
+                         )
+covvplot <- plot_covv %>% ggplot(aes(x = N_samp, y = cov, color = Estimator)) + 
+  geom_point() +
+  scale_color_manual(values = c("#2E9FDF", "#FC4E07")) +
+  xlab("m") + ylab("Coverage") + ggtitle("Interval coverage for V")
---
+++ group8/sim-vis/simfns.R
@@ -0,0 +1,125 @@
+getsim <- function(N_samp, q, qobs){
+  folder <- paste("spec-normal-normal-poisson-mult/n_5/n_", 
+                  as.character(N_samp),
+                  "/q_", as.character(q), "/q_obs_", as.character(qobs), sep = "")
+  
+  #truemu <- matrix()
+  # N_samp= 5
+  data <- readRDS(paste("./sim-data/",folder,"/predict_qpcr_data_jobid_10_ad_0.850000_mt_15_ab_1.rds", sep = ""))
+  # Our model
+  omod <- readRDS(paste("./sim-data/", folder, "/predict_qpcr_mod_jobid_10_ad_0.850000_mt_15_ab_1.rds", sep = ""))
+  osamps <- readRDS(paste("./sim-data/", folder, "/predict_qpcr_samps_jobid_10_ad_0.850000_mt_15_ab_1.rds", sep = ""))
+  # Amy's model
+  amod <- readRDS(paste("./sim-data/", folder, "/predict_qpcr_mod_jobid_10_ad_0.850000_mt_15_ab_1_Amy.rds", sep = ""))
+  asamps <- readRDS(paste("./sim-data/", folder, "/predict_qpcr_samps_jobid_10_ad_0.850000_mt_15_ab_1_Amy.rds", sep = ""))
+  
+  return(list(data = data, omod = omod, osamps = osamps, amod = amod, asamps = asamps))
+}
+
+mus <- function(mods, N_samp, q){
+  omu <- mods$omod[1:(N_subj*N_samp*q), 1]
+  omu <- matrix(omu, ncol = q, byrow = TRUE)
+  amu <- mods$amod[1:(N_subj*N_samp*q), 1]
+  amu <- matrix(amu, ncol = q, byrow = TRUE)
+  nmu <- inits(mods$data$W, mods$data$V, n_samp = N_samp)
+  nmu <- exp(nmu[[1]]$log_mu_tilde)
+  return(list(omu = omu, amu = amu, nmu = nmu))
+}
+
+getrmses <- function(N_samp, q, qobs){
+  mods <- getsim(N_samp, q, qobs)
+  # True mus
+  truemu <- mods$data$mu
+  # Model mu estimates
+  mus <- mus(mods, N_samp, q)
+  
+  odiff <- truemu - mus$omu
+  ormse <- sqrt(1/(N_subj*N_samp*q)*sum(odiff^2))
+  adiff <- truemu - mus$amu
+  armse <- sqrt(1/(N_subj*N_samp*q)*sum(adiff^2))
+  ndiff <- truemu - mus$nmu
+  nrmse <- sqrt(1/(N_subj*N_samp*q)*sum(ndiff^2))
+  return(c(ormse, armse, nrmse))
+}
+
+getrmspes <- function(N_samp, q, qobs){
+  mods <- getsim(N_samp, q, qobs)
+  data <- mods$data
+  vstar <- data$Vstar[,(qobs + 1):q]
+  
+  mus <- mus(mods, N_samp, q)
+  ov <- mus$omu[,(qobs + 1):q]
+  av <- mus$amu[,(qobs + 1):q]
+  nv <- mus$nmu[,(qobs + 1):q]
+  
+  odiff <- vstar - ov
+  ormspe <- sqrt(1/(param*N_subj*q)*sum(odiff^2))
+  adiff <- vstar - av
+  armspe <- sqrt(1/(param*N_subj*q)*sum(adiff^2))
+  ndiff <- vstar - nv
+  nrmspe <- sqrt(1/(param*N_subj*q)*sum(ndiff^2))
+  return(c(ormspe, armspe, nrmspe))
+}
+
+getmucoverage <- function(N_samp, q, qobs){
+  mods <- getsim(N_samp, q, qobs)
+  # True mus
+  truemu <- mods$data$mu
+  
+  # Credible and prediction intervals
+  opostsum <- extract_posterior_summaries(mods$omod, mods$osamps, 1:q)
+  apostsum <- extract_posterior_summaries(mods$amod, mods$asamps, 1:q)
+  
+  ocred <- opostsum$cred_intervals
+  acred <- apostsum$cred_intervals
+  
+  sums <- c()
+  for(i in 1:q){
+    inti <- ocred[,,i]
+    truei <- truemu[,i]
+    indicatori <- (inti[,1] <= truei) & (inti[,2] >= truei)
+    sums <- append(sums, sum(indicatori))
+  }
+  ocovmu <- 1/(N_subj*N_subj*q)*sum(sums)
+  
+  sums <- c()
+  for(i in 1:q){
+    inti <- acred[,,i]
+    truei <- truemu[,i]
+    indicatori <- (inti[,1] <= truei) & (inti[,2] >= truei)
+    sums <- append(sums, sum(indicatori))
+  }
+  acovmu <- 1/(N_subj*N_subj*q)*sum(sums)
+  return(c(ocovmu, acovmu))
+}
+
+getvcoverage <- function(N_samp, q, qobs){
+  mods <- getsim(N_samp, q, qobs)
+  data <- mods$data
+  vstar <- data$Vstar[,(qobs + 1):q]
+  
+  opostsum <- extract_posterior_summaries(mods$omod, mods$osamps, 1:q)
+  apostsum <- extract_posterior_summaries(mods$amod, mods$asamps, 1:q)
+  
+  opred <- opostsum$pred_intervals
+  apred <- apostsum$pred_intervals
+  
+  sums <- c()
+  for(i in (qobs+1):q){
+    inti <- opred[,,i]
+    truei <- vstar[,(i-qobs)]
+    indicatori <- (inti[,1] <= truei) & (inti[,2] >= truei)
+    sums <- append(sums, sum(indicatori))
+  }
+  ocovv <- 1/(N_subj*N_samp*(q-qobs))*sum(sums)
+  
+  sums <- c()
+  for(i in (qobs+1):q){
+    inti <- apred[,,i]
+    truei <- vstar[,(i-qobs)]
+    indicatori <- (inti[,1] <= truei) & (inti[,2] >= truei)
+    sums <- append(sums, sum(indicatori))
+  }
+  acovv <- 1/(N_subj*N_samp*(q-qobs))*sum(sums)
+  return(c(ocovv, acovv))
+}
